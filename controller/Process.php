d46966ce188550dc4f7ffd54e389ecefb7bcbab9
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2015-11-15 00:43:22 +0200

    Process.php bug fixed.

--- controller/Process.php
+++ controller/Process.php
@@ -1,11 +1,10 @@
 <?php
-error_reporting(0); // good enough for demo
+
 require_once("Config.php");
 require_once(Config::HELPER_FILE_USERDB);
 $function = $_POST['function'];
 $data = $_POST['data'];
 $returnData = array();
-
 if (in_array($function, Config::$VALID_FUNCTIONS)) {
 
     $userDBHelper = new User_DB_Helper();
@@ -57,24 +56,24 @@ if (in_array($function, Config::$VALID_FUNCTIONS)) {
             break;
 
         case "nextLevel": // todo: puanı hesaplatıp burada dbye yaz
+        $returnData["levelData"] = file_get_contents(Config::LEVELS_DIRECTORY . "level-2.html");
             if (isset($_COOKIE['platform_id'])) { //todo: platform id'yi cookieye at
                 $socialID = (int)$_COOKIE['platform_id'];
                 $levelCode = (int)$data;
                 $isCodeOK = $userDBHelper->checkLevelCode($levelCode);
-                //if ($isCodeOK !== false) {
+                if ($isCodeOK !== false) {
                     $userDBHelper->increaseLevel($socialID);
                     $newLevel = $userDBHelper->getLevel($socialID);
                     $newLevelFile = Config::LEVELS_DIRECTORY . "level-{$newLevel}.html";
                     if (file_exists($newLevelFile)) {
                         $returnData["levelData"] = file_get_contents($newLevelFile);
                     }
-                //}
+                }
             }
             break;
 
     }
 
 }
-
 if (!empty($returnData))
     echo json_encode($returnData);
