0e10f74bde31bbfa9f2d03ddccf9dc383c5ced97
Mahmut Karaca <mahmutkaraca95@gmail.com>
2015-11-15 06:32:11 +0200

    wow

--- controller/Config.php
+++ controller/Config.php
@@ -4,13 +4,13 @@ class Config
 {
     const DB_HOST = "localhost";
     const DB_USERNAME = "root";
-    const DB_PASSWORD = "1233211";
+    const DB_PASSWORD = ""; //220513Yigit
     const DB_DATABASE = "eset_hackathon";
 
-    const BASE_URL = "/Library/WebServer/Documents/ESETGuvenlikOyunu/";
-    const HELPER_DIRECTORY = self::BASE_URL . "Helpers/";
-    const HELPER_FILE_USERDB = self::HELPER_DIRECTORY . "users_db_helper.php";
-    const LEVELS_DIRECTORY = self::BASE_URL . "views/levels/";
+    const BASE_URL = "C:/wamp/www/ESETGuvenlikOyunu/";
+    const HELPER_DIRECTORY = "C:/wamp/www/ESETGuvenlikOyunu/Helpers/";
+    const HELPER_FILE_USERDB = "C:/wamp/www/ESETGuvenlikOyunu/Helpers/users_db_helper.php";
+    const LEVELS_DIRECTORY = "C:/wamp/www/ESETGuvenlikOyunu/views/levels/";
 
     const MAX_POINT = 20;
     const MIN_POINT = 0;
