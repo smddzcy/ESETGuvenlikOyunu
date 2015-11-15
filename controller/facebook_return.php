500a439b7ba836a523b2e71d107766694340dbce
Samed Düzçay <samedduzcay@gmail.com>
2015-11-15 03:40:21 +0200

    updated

--- controller/facebook_return.php
+++ controller/facebook_return.php
@@ -1,18 +1,18 @@
-<?php  
+<?php
 
 $code = $_GET["code"];
 
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://graph.facebook.com/v2.3/oauth/access_token?".
-    "client_id=1665895287021960".
-   "&redirect_uri=http://localhost/ESETGuvenlikOyunu/controller/facebook_return.php".
-   "&client_secret=c636846ef008ff87150aa3883622e298".
-   "&code=".$code);
+curl_setopt($curl, CURLOPT_URL, "https://graph.facebook.com/v2.3/oauth/access_token?" .
+    "client_id=1665895287021960" .
+    "&redirect_uri=http://localhost/ESETGuvenlikOyunu/controller/facebook_return.php" .
+    "&client_secret=c636846ef008ff87150aa3883622e298" .
+    "&code=" . $code);
 
 curl_setopt_array($curl, array(
-	CURLOPT_SSL_VERIFYPEER => false,
-	CURLOPT_SSL_VERIFYHOST => false,
-	CURLOPT_RETURNTRANSFER => true
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false,
+    CURLOPT_RETURNTRANSFER => true
 ));
 
 $json = json_decode(curl_exec($curl));
@@ -22,11 +22,11 @@ checkPermissions($json);
 
 $curl = curl_init();
 
-curl_setopt($curl, CURLOPT_URL, "https://graph.facebook.com/v2.5/me?fields=id,name,email&access_token=".$json->access_token);
+curl_setopt($curl, CURLOPT_URL, "https://graph.facebook.com/v2.5/me?fields=id,name,email&access_token=" . $json->access_token);
 curl_setopt_array($curl, array(
-	CURLOPT_SSL_VERIFYPEER => false,
-	CURLOPT_SSL_VERIFYHOST => false,
-	CURLOPT_RETURNTRANSFER => true
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false,
+    CURLOPT_RETURNTRANSFER => true
 ));
 
 $user_data = json_decode(curl_exec($curl));
@@ -34,58 +34,59 @@ $user_data = json_decode(curl_exec($curl));
 curl_close($curl);
 
 $data = array(
-	"function" => "addUser",
-	"data" => array(
-		"name" => $user_data->name,
-		"platform" => 0,
-		"platform_id" => $user_data->id,
-		"email" => $user_data->email
-	)
+    "function" => "addUser",
+    "data" => array(
+        "name" => $user_data->name,
+        "platform" => 0,
+        "platform_id" => $user_data->id,
+        "email" => $user_data->email
+    )
 );
 
 $curl = curl_init();
 
 curl_setopt($curl, CURLOPT_URL, "http://localhost/ESETGuvenlikOyunu/controller/Process.php");
 curl_setopt_array($curl, array(
-	CURLOPT_SSL_VERIFYPEER => false,
-	CURLOPT_SSL_VERIFYHOST => false,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_POST => true,
-	CURLOPT_POSTFIELDS => http_build_query($data)
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query($data)
 ));
 
 echo curl_exec($curl);
 
-//header("Location: http://localhost/ESETGuvenlikOyunu/oyun");
-//die();
 
-function rerequestPermissions() {
-	header("Location: https://www.facebook.com/dialog/oauth?".
-    "client_id=1665895287021960&".
-    "redirect_uri=http://localhost/ESETGuvenlikOyunu/facebook_return.php&".
-    "auth_type=rerequest&".
-    "scope=email,user_friends");
+function rerequestPermissions()
+{
+    header("Location: https://www.facebook.com/dialog/oauth?" .
+        "client_id=1665895287021960&" .
+        "redirect_uri=http://localhost/ESETGuvenlikOyunu/facebook_return.php&" .
+        "auth_type=rerequest&" .
+        "scope=email,user_friends");
 }
 
-function checkPermissions($json) {
-	$curl = curl_init();
+function checkPermissions($json)
+{
+    $curl = curl_init();
 
-	curl_setopt($curl, CURLOPT_URL, "https://graph.facebook.com/v2.5/me/permissions?access_token=".$json->access_token);
-	curl_setopt_array($curl, array(
-		CURLOPT_SSL_VERIFYPEER => false,
-		CURLOPT_SSL_VERIFYHOST => false,
-		CURLOPT_RETURNTRANSFER => true
-	));
+    curl_setopt($curl, CURLOPT_URL, "https://graph.facebook.com/v2.5/me/permissions?access_token=" . $json->access_token);
+    curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_RETURNTRANSFER => true
+    ));
 
-	$perms = json_decode(curl_exec($curl));
+    $perms = json_decode(curl_exec($curl));
 
-	foreach ($perms->data as $key => $value) {
-		if($value->status !== "granted") {
-			rerequestPermissions();
-		}
-	}
+    foreach ($perms->data as $key => $value) {
+        if ($value->status !== "granted") {
+            rerequestPermissions();
+        }
+    }
 }
 
-
+header("Location: http://localhost/ESETGuvenlikOyunu/oyun");
+die();
 
 ?>
